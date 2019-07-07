5b7521cff4c567008ec15387c3cfe0123879622c
Vincent Scavetta <scavet64@students.rowan.edu>
2019-07-07 04:37:13 -0400

    Enabling Async for spring

--- OrganizerBackend/src/main/java/com/scavettapps/organizer/OrganizerBackendApplication.java
+++ OrganizerBackend/src/main/java/com/scavettapps/organizer/OrganizerBackendApplication.java
@@ -2,7 +2,9 @@ package com.scavettapps.organizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class OrganizerBackendApplication {
 
