6073745b4c99ac529578b76ad73abfcd489825a4
Vincent Scavetta <scavet64@students.rowan.edu>
2019-07-25 23:24:20 -0400

    making some enhancements to the tag page

--- organizer-client/src/app/tag/tag.page.ts
+++ organizer-client/src/app/tag/tag.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TagModel } from './tagModel';
 
 @Component({
   selector: 'app-tag',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tag.page.scss'],
 })
 export class TagPage implements OnInit {
+
+  tags: TagModel[] = [
+    new TagModel('testing 1'),
+    new TagModel('testing 2'),
+    new TagModel('testing 3'),
+    new TagModel('testing 4'),
+    new TagModel('testing 5'),
+    new TagModel('testing 6'),
+    new TagModel('testing 7'),
+    new TagModel('testing 8')
+  ];
 
   constructor() { }
 
---
+++ organizer-client/src/app/tag/tagModel.ts
@@ -0,0 +1,7 @@
+export class TagModel {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
