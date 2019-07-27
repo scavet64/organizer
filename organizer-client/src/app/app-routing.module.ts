51c11b20e5086aee312093cc158893ccd039bc99
Vincent Scavetta <scavet64@students.rowan.edu>
2019-07-27 05:29:24 -0400

    Remove generated page

--- organizer-client/src/app/app-routing.module.ts
+++ organizer-client/src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
   { path: 'tag', loadChildren: './tag/tag.module#TagPageModule' },
   { path: 'tag/modification', component: CreateEditPage },
   { path: 'create', loadChildren: './tag/create-edit/create-edit.module#CreatePageModule' }
